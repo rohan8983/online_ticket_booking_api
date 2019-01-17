16578b43f502d44328382e95a1643a6863a2d13c
rohan8983 <rohan@peoplelinkvc.com>
2019-01-17 12:47:56 +0530

    done login and registration api

--- App/controllers/login.js
+++ App/controllers/login.js
@@ -1,12 +1,18 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 module.exports = async function(req, res, next) {
   let user = await User.findOne({ email: req.body.email });
   if (!user) return res.status(400).send("Invalid Username or Password");
 
+  //validating password with
   const vallidPassword = await bcrypt.compare(req.body.password, user.password);
   if (!vallidPassword)
     return res.status(400).send("Invalid Username or Password");
-  res.send(true);
+
+  //genrating token
+  const token = jwt.sign({ _id: user._id }, "onlineBooking_jwtPrivateKey");
+  res.status(200).send({ status: true, token });
 };
--- App/startup/db.js
+++ App/startup/db.js
@@ -1,12 +1,14 @@
 const winston = require("winston");
 const mongoose = require("mongoose");
+const config = require("config");
 
 module.exports = function() {
+  const db = config.get("db");
   mongoose
     .connect(
-      "mongodb://localhost:27017/online_booking",
+      db,
       { useNewUrlParser: true }
     )
-    .then(() => winston.info("MongoDB connected successfully..."));
+    .then(() => winston.info(`MongoDB connected successfully to ${db}...`));
 };
 // "mongodb://rohan:rohan123@ds129914.mlab.com:29914/online_booking",
